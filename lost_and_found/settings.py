051f1287a33a5ca9a8f92090209152ffbce67fe5
PavanPavu <reddyupavankumar98@gmail.com>
2024-12-09 11:59:26 +0000

    secret key

--- lost_and_found/settings.py
+++ lost_and_found/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECURITY_KEY')
+# SECRET_KEY = config('SECURITY_KEY')
+SECRET_KEY = 'django-insecure-he4m%kr^_zx7kn1qnikm=9=7rgtqg#+s7%u6&a88p4d(c&-49x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
